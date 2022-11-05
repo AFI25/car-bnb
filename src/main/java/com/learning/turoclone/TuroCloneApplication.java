2d7d558c70c97a1efd77a25bbe4b60c8a288454d
AFI25 <88861177+AFI25@users.noreply.github.com>
2022-11-05 19:45:55 -0400

    Add files via upload
    initial commit

---
+++ src/main/java/com/learning/turoclone/TuroCloneApplication.java
@@ -0,0 +1,13 @@
+package com.learning.turoclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuroCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuroCloneApplication.class, args);
+	}
+
+}
---
+++ turo-clone/src/test/java/com/learning/turoclone/TuroCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.turoclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuroCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
